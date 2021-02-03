7c1eebd1469c823fcada21cbb073506d2362f94d
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2021-02-03 10:57:15 +0100

    feat(vm-series): identity/principal outputs

--- modules/vm-series/outputs.tf
+++ modules/vm-series/outputs.tf
@@ -3,3 +3,13 @@ output "ip_addresses" {
   description = "VM-Series management IP addresses."
   value       = { for k, v in var.instances : k => azurerm_network_interface.nic-fw-mgmt[k].ip_configuration[0].private_ip_address }
 }
+
+output "identity_ids" {
+  description = "VM-Series Azure user-assigned identities. Map of lists: key is the same as virtual machine key, the value is a list of Azure identities. Usable only if `identity_type` contains UserAssigned."
+  value       = { for k, v in var.instances : k => azurerm_virtual_machine.this[k].identity[0].identity_ids if var.identity_type != null && var.identity_type != "" }
+}
+
+output "principal_id" {
+  description = "Azure Service Principals for each of the created VM-Series. Map's key is the same as virtual machine key, the value is an oid of a Service Principal. Usable only if `identity_type` contains SystemAssigned."
+  value       = { for k, v in var.instances : k => azurerm_virtual_machine.this[k].identity[0].principal_id if var.identity_type != null && var.identity_type != "" }
+}
