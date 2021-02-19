a90bdf34e3b241ccc49d930ec5ec0275ab64ea66
Damian Fedeczko <dfedeczko@paloaltonetworks.com>
2021-02-19 14:52:19 +0100

    Change input variables structure

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,51 +1,8 @@
-//Resource Group
-resource "azurerm_resource_group" "this" {
-  count = var.create_resource_group ? 1 : 0
-
-  name     = var.resource_group_name
-  location = var.location
-}
-
 data "azurerm_resource_group" "this" {
-  name       = var.resource_group_name
-  depends_on = [azurerm_resource_group.this]
+  name = var.resource_group_name
 }
 
-//Network Security Groups and Rules
-resource "azurerm_network_security_group" "this" {
-  count = var.create_network_security_group ? 1 : 0
-
-  name                = var.network_security_group_name
-  location            = data.azurerm_resource_group.this.location
-  resource_group_name = data.azurerm_resource_group.this.name
-
-  dynamic "security_rule" {
-    for_each = var.rules
-
-    content {
-      name                       = security_rule.value["name"]
-      priority                   = security_rule.value["priority"]
-      direction                  = security_rule.value["direction"]
-      access                     = security_rule.value["access"]
-      protocol                   = security_rule.value["protocol"]
-      source_port_range          = security_rule.value["source_port_range"]
-      destination_port_range     = security_rule.value["destination_port_range"]
-      source_address_prefix      = security_rule.value["source_address_prefix"]
-      destination_address_prefix = security_rule.value["destination_address_prefix"]
-    }
-  }
-}
-
-data "azurerm_network_security_group" "this" {
-  name                = var.network_security_group_name
-  resource_group_name = data.azurerm_resource_group.this.name
-  depends_on          = [azurerm_network_security_group.this]
-}
-
-//VNet and Subnet association
 resource "azurerm_virtual_network" "this" {
-  count = var.create_virtual_network ? 1 : 0
-
   name                = var.virtual_network_name
   location            = data.azurerm_resource_group.this.location
   resource_group_name = data.azurerm_resource_group.this.name
@@ -57,42 +14,51 @@ resource "azurerm_virtual_network" "this" {
     content {
       name           = subnet.value["name"]
       address_prefix = subnet.value["address_prefix"]
-      security_group = coalesce(subnet.value["security_group"], data.azurerm_network_security_group.this.id)
+      security_group = try(subnet.value["security_group"], azurerm_network_security_group.this.id)
     }
   }
   depends_on = [azurerm_network_security_group.this]
 }
 
-# data "azurerm_virtual_network" "this" {
-#   name                = var.virtual_network_name
-#   resource_group_name = data.azurerm_resource_group.this.name
-#   depends_on          = [azurerm_virtual_network.this]
-# }
-
-//Route Table creation
-resource "azurerm_route_table" "this" {
-  name                = var.route_table_name
+resource "azurerm_network_security_group" "this" {
+  name                = var.network_security_group.name
   location            = data.azurerm_resource_group.this.location
   resource_group_name = data.azurerm_resource_group.this.name
-  disable_bgp_route_propagation = false
 
-  dynamic "route" {
-    for_each = var.routes
+  dynamic "security_rule" {
+    for_each = var.network_security_group.rules
 
     content {
-      name           = route.value["name"]
-      address_prefix = route.value["address_prefix"]
-      next_hop_type  = route.value["next_hop_type"]
-    } 
-  }
-
-  tags = {
-    environment = "Production"
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+      source_port_range          = security_rule.value.source_port_range
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefix      = security_rule.value.source_address_prefix
+      destination_address_prefix = security_rule.value.destination_address_prefix
+    }
   }
 }
 
-//Route Table association
-resource "azurerm_subnet_route_table_association" "this" {
-  subnet_id      = azurerm_virtual_network.this.*.id[0]
-  route_table_id = azurerm_route_table.this.id
+resource "azurerm_route_table" "this" {
+  name                          = var.route_table.name
+  location                      = data.azurerm_resource_group.this.location
+  resource_group_name           = data.azurerm_resource_group.this.name
+  disable_bgp_route_propagation = false
+
+  dynamic "route" {
+    for_each = var.route_table.routes
+
+    content {
+      name           = route.value.name
+      address_prefix = route.value.address_prefix
+      next_hop_type  = route.value.next_hop_type
+    }
+  }
+
+  tags = {
+    environment = "dftest"
+  }
 }
--- modules/vnet/variables.tf
+++ modules/vnet/variables.tf
@@ -1,21 +1,19 @@
-//Options enabling a Greenfield deployment
-variable "create_resource_group" {
-  description = "Enable this option if you want to create a new Resource Group, default value set to `true`"
-  default     = true
-}
-variable "create_network_security_group" {
-  description = "Enable this option if you want to create a new Network Security Group, default value set to `true`"
-  default     = true
+variable "resource_group_name" {
+  description = "Name of the Resource Group to use."
+  type        = string
 }
 
-variable "create_virtual_network" {
-  description = "Enable this option if you want to create a new VNet, default value set to `true`"
-  default     = true
+variable "subnets" {
+  description = "Definition of subnets to create."
+  default     = {}
 }
 
-//Resource variables
-variable "network_security_group_name" {
-  description = "Name of the Network Security Group to create."
+variable "network_security_group" {
+  description = "Network Security Groups to create."
+}
+
+variable "route_table" {
+  description = "Route Tables to create."
 }
 
 variable "virtual_network_name" {
@@ -26,34 +24,4 @@ variable "address_space" {
   description = "Address space for VNet."
   type        = list(string)
   default     = ["10.0.0.0/16"]
-}
-
-variable "subnets" {
-  description = "Definition of subnets to create."
-  default     = {}
-}
-
-variable "rules" {
-  description = "Definition of subnets to create."
-  default     = {}
-}
-
-variable "location" {
-  description = "Location of the resources that will be deployed."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Name of the Resource Group to create."
-  type        = string
-}
-
-variable "route_table_name" {
-  description = "Name of the Route Table to create."
-  type        = string
-}
-
-variable "routes" {
-  description = "Definition of subnets to create."
-  default     = {}
 }
